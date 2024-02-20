9e875bc1e6448b049a8d58e4e9db4cd7cadb796d
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2024-02-20 12:30:18 -0300

    Desenvolvendo o component Conversor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ConversorMoeda moedaA="USD" moedaB="BRL"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ConversorMoeda from "./components/Conversor.vue";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {ConversorMoeda
   }
-}
+};
 </script>
 
 <style>
